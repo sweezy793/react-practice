0354b9312cc1adf58081a94ed81d864ee152950d
sweezy793 <sarthaks793@gmail.com>
2018-08-12 22:28:40 +0530

    Done structuring

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -48,7 +48,21 @@ class Menu extends Component{
 
     render(){
 
-        const menu;
+        const menu=this.state.dishes.map((dish)=>{
+            return(
+                <div key={dish.id} className="col-12 mt-5">
+                    <Media tag="li">
+                        <Media left middle>
+                            <Media object src={dish.image} alt={dish.name}/>
+                        </Media>    
+                        <Media body className="ml-5">
+                            <Media heading>{dish.name}</Media>
+                            <p>{dish.description}</p>
+                        </Media>
+                    </Media>
+                </div>
+            );
+        });
 
         return(
             <div className="container">
