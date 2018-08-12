e98bc24be34761f74f80fb875839e447720c9997
sweezy793 <sarthaks793@gmail.com>
2018-08-12 22:46:51 +0530

    More changes

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
