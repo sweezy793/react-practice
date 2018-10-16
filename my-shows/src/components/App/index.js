5b9cd581bd657d0fecdf83833fb09e35b2eb5075
sweezy793 <sarthaks793@gmail.com>
2018-10-16 11:31:50 +0530

    Container

--- my-shows/src/components/App/index.js
+++ my-shows/src/components/App/index.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Intro from '../Intro';
+import Series from '../../containers/Series'
 import 'whatwg-fetch';
 
 class App extends Component {
 
-  state={
-    series:[]
-  }
-
-  componentDidMount(){
-    
-    fetch('http://api.tvmaze.com/search/shows?q=daredevil')
-    .then(response=>response.json())
-    .then(json=>this.setState({series:json}))
-
-  }
+  
 
   render() {
     return (
@@ -24,7 +15,7 @@ class App extends Component {
           <h1>My Shows</h1>  
         </header>
         <Intro message="Here you can find all of your favourite tv shows"/>
-        The length of series array - {this.state.series.length}
+        <Series/>
       </div>
     );
   }
