a952436c25cb3471cdb69d1689ec0de7750420e6
sweezy793 <sarthaks793@gmail.com>
2018-10-16 23:47:14 +0530

    Using routes

--- my-shows/src/containers/SingleSeries/index.js
+++ my-shows/src/containers/SingleSeries/index.js
@@ -1,11 +1,23 @@
 import React,{Component} from 'react';
-
+import Loader from '../../components/Loader';
 class SingleSeries extends Component{
-    render(){
+    state={
+        show:null
+    }
 
+    componentDidMount(){
+        const {id}=this.props.match.params;
+        fetch(`http://api.tvmaze.com/shows/${id}?embed=episodes`)
+        .then(response=>response.json())
+        .then(json=>this.setState({show:json}));
+    }
+    render(){
+        const {show}=this.state;
+        console.log(show);
         return(
             <div>
-                <p>Single Series id:{this.props.match.params.id}</p>
+                {show===null&&<Loader/>}
+                {show!==null&&<p>Show has been loaded</p>}
             </div>
         )
     }
