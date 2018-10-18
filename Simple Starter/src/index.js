d58021789e7f3770b4524ba10f2ad8fcff14ef8e
sweezy793 <sarthaks793@gmail.com>
2018-10-19 01:19:41 +0530

    API works

--- Simple Starter/src/index.js
+++ Simple Starter/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import SearchBar from '../components/search_bar'
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/search_bar'
 
 const API_KEY='AIzaSyCFBBIcBhMZ0aG0Gc2Gab0hGoBC60nnkgg';
+
+YTSearch({key:API_KEY,term:'surfboards'},function(data){
+    console.log(data);
+});
 
 const App=()=>{
     return (
