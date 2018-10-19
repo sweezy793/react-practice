22c13c1e652be39b6c4a85aab810000b064e4e37
sweezy793 <sarthaks793@gmail.com>
2018-10-20 02:14:17 +0530

    iFrame

--- Simple Starter/src/index.js
+++ Simple Starter/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 
 const API_KEY='AIzaSyCFBBIcBhMZ0aG0Gc2Gab0hGoBC60nnkgg';
 
@@ -22,6 +23,7 @@ class App extends Component{
     return (
         <div>
             <SearchBar/>
+            <VideoDetail video={this.state.videos[4]}/>
             <VideoList videos={this.state.videos}/>
         </div>
     );
