a34ccdcac06df9c8cc91189807ec392718879d0f
sweezy793 <sarthaks793@gmail.com>
2018-10-19 18:16:19 +0530

    Refactoring

--- Simple Starter/src/index.js
+++ Simple Starter/src/index.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
 
 const API_KEY='AIzaSyCFBBIcBhMZ0aG0Gc2Gab0hGoBC60nnkgg';
 
-YTSearch({key:API_KEY,term:'surfboards'},function(data){
-    console.log(data);
-});
 
-const App=()=>{
+
+class App extends Component{
+    constructor(props){
+        super(props);
+
+        this.state={videos:[]}
+
+        YTSearch({key:API_KEY,term:'surfboards'},(data)=>{
+            this.setState({videos:data});
+        });
+    }
+    render(){   
     return (
         <div>
             <SearchBar/>
         </div>
     );
+    }
 }
 
 ReactDOM.render(<App/>,document.querySelector('.container'));
