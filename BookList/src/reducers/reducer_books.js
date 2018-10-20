6d8241087d0586d75e6f3cc3781031f5526c7eb6
sweezy793 <sarthaks793@gmail.com>
2018-10-21 00:02:01 +0530

    Some changes

---
+++ BookList/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title:'The Linux Bible'},
+        {title:'Good Omens'},
+        {title:'The Leftovers'},
+        {title:'The Alchemist'}
+    ]
+}
