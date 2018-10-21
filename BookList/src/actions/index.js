05872a8b5b1e41aa2031356fc080b9db8afac126
sweezy793 <sarthaks793@gmail.com>
2018-10-22 02:15:51 +0530

    Taking actions into reducers

--- BookList/src/actions/index.js
+++ BookList/src/actions/index.js
@@ -1,3 +1,7 @@
 export function  selectBook(book){
-    console.log('Book selected:',book.title);
+    //selectBook is an Action Creator, it needs to return an action, an object with type property
+    return{
+        type:'BOOK_SELECTED',
+        payload:book
+    }
 }
