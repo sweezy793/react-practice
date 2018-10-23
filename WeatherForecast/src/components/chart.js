247ec861e22f33074967cae3ace97cbef40006fd
sweezy793 <sarthaks793@gmail.com>
2018-10-23 23:38:36 +0530

    Sparklines for all three conditions

---
+++ WeatherForecast/src/components/chart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Sparklines, SparklinesLine} from 'react-sparklines';
+
+
+export default (props)=>{
+    return (
+        <div>
+          <Sparklines height={40} width={100} data={props.data}>
+            <SparklinesLine color={props.color}/>
+          </Sparklines>
+        </div>
+    );
+}
