06aefc2c2bdaa08f80dc7a305ad40db2018230cb
sweezy793 <sarthaks793@gmail.com>
2018-11-08 19:57:27 +0530

    Link tag

--- React Blog/src/components/posts_index.js
+++ React Blog/src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React ,{Component} from 'react';
 import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
+import {Link} from 'react-router-dom';
 import _ from 'lodash';
 
 
@@ -23,6 +24,11 @@ class PostsIndex extends Component{
     {
         return(
             <div>
+                <div className="text-xs-right">
+                    <Link className="btn btn-primary" to="/posts/new"> {/* Link acts like anchor tag and to acts like href */}
+                        Add a post
+                    </Link>
+                </div>
                 <h3>Posts</h3>
                 <ul className="list-group">
                     {this.renderPosts()}
--- React Blog/src/index.js
+++ React Blog/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import {BrowserRouter,Route} from 'react-router-dom';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
 import PostsIndex from './components/posts_index'
+import PostsNew from './components/posts_new';
 import reducers from './reducers';
 import promise from 'redux-promise';
 
@@ -15,7 +16,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
     <div>
-      <Route path="/" component={PostsIndex}/>
+      <Switch>  
+        <Route path="/posts/new" component={PostsNew}/>
+        <Route path="/" component={PostsIndex}/>
+      </Switch>
     </div>    
     </BrowserRouter>
   </Provider>
