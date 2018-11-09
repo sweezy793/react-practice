0546aa25757810d11b3e0880632412604c039d3d
sweezy793 <sarthaks793@gmail.com>
2018-11-10 01:28:14 +0530

    Done

--- React Blog/src/actions/index.js
+++ React Blog/src/actions/index.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 import rootReducer from '../reducers';
 
+
 export const FETCH_POSTS='fetch_posts';
 export const FETCH_POST='fetch_post'
 export const CREATE_POST='create_post';
+export const DELETE_POST='delete_post';
 
 const ROOT_URL='http://reduxblog.herokuapp.com/api';
 const API_KEY='?key=SS';
@@ -34,4 +36,14 @@ export function fetchPost(id){
         type: FETCH_POST,
         payload:request
     };
+}
+
+export function deletePost(id,callback){
+    const request=axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+    .then(()=>callback());
+
+    return{
+        type:DELETE_POST,
+        payload:id
+    }
 }
